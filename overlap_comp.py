9ff5f73491c21a1d615123776fd0d3e6415ce8f5
PeytonSpreacker <peyton@spreacker.com>
2022-04-08 13:15:01 -0500

    initial commit

---
+++ overlap_comp.py
@@ -0,0 +1,13 @@
+# allows for random list generation in solution 2
+import random
+
+# sample lists
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+overlap = list()
+
+# solution 2: randomly generates two lists of integers
+# list1 = random.sample(range(0,101), 10)
+# list2 = random.sample(range(0,101), 10)
+
